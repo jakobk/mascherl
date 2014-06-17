89aab921604a41e554b0bf4e55574fc0791a9383
Jakob Korherr <jakob.korherr@gmail.com>
2014-06-17 14:49:32 +0200

    initial maven project structure + gitignore

---
+++ project/scaleable-web-framework/src/main/java/at/tuwien/jakobk/App.java
@@ -0,0 +1,13 @@
+package at.tuwien.jakobk;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
