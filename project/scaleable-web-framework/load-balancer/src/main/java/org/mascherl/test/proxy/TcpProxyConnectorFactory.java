029d5ba1583aac86ef359630a12e44c4386915a1
Jakob Korherr <jakob.korherr@gmail.com>
2015-04-09 17:13:36 +0200

    add NIO based load balancer, based on a github project from terma

---
+++ project/scaleable-web-framework/load-balancer/src/main/java/org/mascherl/test/proxy/TcpProxyConnectorFactory.java
@@ -0,0 +1,40 @@
+/*
+Copyright 2012 Artem Stasuk
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+ */
+
+package org.mascherl.test.proxy;
+
+import org.mascherl.test.balancer.RoundRobinServerSelector;
+import org.mascherl.test.nio.TcpServerHandler;
+import org.mascherl.test.nio.TcpServerHandlerFactory;
+
+import java.nio.channels.SocketChannel;
+
+class TcpProxyConnectorFactory implements TcpServerHandlerFactory {
+
+    private final TcpProxyConfig config;
+    private final RoundRobinServerSelector serverSelector;
+
+    public TcpProxyConnectorFactory(TcpProxyConfig config, RoundRobinServerSelector serverSelector) {
+        this.config = config;
+        this.serverSelector = serverSelector;
+    }
+
+    @Override
+    public TcpServerHandler create(final SocketChannel clientChannel) {
+        return new TcpProxyConnector(clientChannel, config, serverSelector);
+    }
+
+}
