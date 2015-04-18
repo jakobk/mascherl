243f445b78b3768b76bd07724df122619c37dc01
Jakob Korherr <jakob.korherr@gmail.com>
2015-04-18 15:29:47 +0200

    add license headers

--- project/scaleable-web-framework/test/test-web-spring-hibernate/src/main/java/org/mascherl/example/service/TestDataService.java
+++ project/scaleable-web-framework/test/test-web-spring-hibernate/src/main/java/org/mascherl/example/service/TestDataService.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2015, Jakob Korherr
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.mascherl.example.service;
 
 import org.mascherl.example.domain.Mail;
@@ -26,15 +41,15 @@ public class TestDataService {
     @PostConstruct
     public void init() {
         loginService.createNewUser(new User("Jakob", "Korherr", "jakob.korherr@gmail.com"), "pwd");
-        loginService.createNewUser(new User("Steffi", "Pollmann", "SPollmann@gmx.at"), "pwd");
+        loginService.createNewUser(new User("Steffi", "Pollmann", "steffi.pollmann@gmail.com"), "pwd");
 
         User userJakobKorherr = loginService.login("jakob.korherr@gmail.com", "pwd");
-        User userSteffiPollmann = loginService.login("SPollmann@gmx.at", "pwd");
+        User userSteffiPollmann = loginService.login("steffi.pollmann@gmail.com", "pwd");
 
         sendMailService.sendMail(
                 new Mail(
                         new MailAddress("jakob.korherr@gmail.com"),
-                        Collections.singleton(new MailAddress("SPollmann@gmx.at")),
+                        Collections.singleton(new MailAddress("steffi.pollmann@gmail.com")),
                         Collections.singleton(new MailAddress("some.other.address@asdf.com")),
                         null,
                         "Test subject",
@@ -43,7 +58,7 @@ public class TestDataService {
         sendMailService.sendMail(
                 new Mail(
                         new MailAddress("jakob.korherr@gmail.com"),
-                        Collections.singleton(new MailAddress("SPollmann@gmx.at")),
+                        Collections.singleton(new MailAddress("steffi.pollmann@gmail.com")),
                         null,
                         null,
                         "Test subject 2",
@@ -51,7 +66,7 @@ public class TestDataService {
                 userJakobKorherr);
         sendMailService.sendMail(
                 new Mail(
-                        new MailAddress("SPollmann@gmx.at"),
+                        new MailAddress("steffi.pollmann@gmail.com"),
                         Collections.singleton(new MailAddress("jakob.korherr@gmail.com")),
                         null,
                         null,
